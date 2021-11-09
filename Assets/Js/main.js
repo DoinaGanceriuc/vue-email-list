cc2ff6446395d37f01f91d93806d087ce975207a
DoinaGanceriuc <doinaganceriuc@gmail.com>
2021-11-09 14:36:08 +0100

    complete 10 random mail

--- Assets/Js/main.js
+++ Assets/Js/main.js
@@ -10,18 +10,32 @@ Far comparire gli indirizzi email solamente quando sono stati tutti generati. */
 const app = new Vue ({
     el: "#root",
     data: {
-        randomMail: ""
+        randomMail: "",
+        listRandomMail: []
 
     },
     mounted() {
-        axios
-        .get("https://flynn.boolean.careers/exercises/api/random/mail")
-        .then(response => {
-            //console.log(response);
-            //console.log(response.data);
-            //console.log(response.data.response);
 
-            this.randomMail = response.data.response
+        let i = 0;
+        while (i < 10) {
+            axios
+                .get("https://flynn.boolean.careers/exercises/api/random/mail")
+                .then(response => {            
+                    this.randomMail = response.data.response
+                    //console.log(this.randomMail);
+                    if(!this.listRandomMail.includes(this.randomMail)) {
+                        this.listRandomMail.push(this.randomMail)
+                        //console.log(this.listRandomMail);
+                    }
+                    
+             
         })
+            
+ 
+            i++;
+        }
+        
+
+       
     }
 })
